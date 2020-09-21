2f709eeb35c22d8ff812992eadb257406c7a26ba
Haasin Farooq <70452746+haasin-farooq@users.noreply.github.com>
2020-09-22 01:50:32 +0500

    Updated script.js
    Features added:
    - Saving and retrieving data from local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 let addTaskButton = document.querySelector('.add-task-button');
 let taskInput = document.querySelector('.task-input');
-let taskContainer = document.querySelector('.task-container')
+let taskContainer = document.querySelector('.task-container');
+let tasks = [];
 
 class Task {
   // Constructor that calls the createTask function
-  constructor(taskTitle, time) {
-    this.createTask(taskTitle, time);
+  constructor(taskTitle, time, completed) {
+    this.taskTitle = taskTitle;
+    this.taskTime = time;
+    this.completed = completed;
   }
 
   // Function to create the task div
@@ -74,6 +77,10 @@ class Task {
     taskRow2Div.appendChild(completeTaskButton);
     taskRow2Div.appendChild(editTaskButton);
 
+    this.completed ? taskDiv.classList.add('completed') : taskDiv.classList.remove('completed');
+
+    taskInput.value = "";
+
     editTaskButton.addEventListener('click', () => this.editTask(taskTitleInput));
 
     completeTaskButton.addEventListener('click', () => this.completeTask(taskDiv));
@@ -84,16 +91,26 @@ class Task {
   // Function that toggles the disabled property of taskTitleInput
   editTask(taskTitleInput) {
     taskTitleInput.disabled = !taskTitleInput.disabled;
+    let index = tasks.findIndex((task) => task.taskTitle === this.taskTitle); // Testing function that returns index of the first element that satisfies the condition
+    tasks[index].taskTitle = taskTitleInput.value;
+    saveTasksLocally();
   }
 
   // Function that adds/removes the class completed to the taskDiv
   completeTask(taskDiv) {
+    this.completed = !this.completed;
     taskDiv.classList.toggle('completed'); // opacity = 0.5
+    let index = tasks.findIndex(task => task.taskTitle === this.taskTitle);
+    tasks[index].completed = !tasks[index].completed;
+    saveTasksLocally();
   }
 
   // Function that removes the taskDiv from the taskContainer
   deleteTask(taskDiv) {
     taskContainer.removeChild(taskDiv);
+    let index = tasks.findIndex(task => task.taskTitle === this.taskTitle);
+    tasks.splice(index, 1); // Remove 1 element from position 'index'
+    saveTasksLocally();
   }
 }
 
@@ -107,11 +124,45 @@ window.addEventListener('keyup', (e) => {
   }
 });
 
+// Initialize
+getTasksLocally();
+listTasks();
+
+
+// Functions
+
 // Function that creates a new Task object
 function addTask() {
   if (taskInput.value) {
     var currentDate = new Date();
     var dateTime = currentDate.toLocaleDateString() + " @ " + currentDate.toLocaleTimeString();
-    new Task(taskInput.value, dateTime);
+    var task = new Task(taskInput.value, dateTime, false);
+    task.createTask(task.taskTitle, task.taskTime, task.completed);
+    tasks.push(task);
+    saveTasksLocally();
   }
 }
+
+// Function that creates all the tasks in the tasks array
+function listTasks() {
+  for(let i = 0; i < tasks.length; i++) {
+    var task = new Task(tasks[i].taskTitle, tasks[i].taskTime, tasks[i].completed);
+    task.createTask(task.taskTitle, task.taskTime, task.completed);
+  }
+}
+
+// Function that saves tasks to local storage
+function saveTasksLocally() {
+  var strTasks = JSON.stringify(tasks);
+  localStorage.setItem("tasks", strTasks);
+}
+
+// Function that gets data from local storage
+function getTasksLocally() {
+  var strTasks = localStorage.getItem("tasks");
+  tasks = JSON.parse(strTasks);
+  if(!tasks) {
+    tasks = [];
+  }
+}
+
